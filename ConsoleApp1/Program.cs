ba4001a05de13d9bc25b87d2f3bcd4616f97b9df
Nika <Nika@LAPTOP-PM4D8H38>
2024-12-20 23:56:17 +0700

    литералы(5,2) делаем вещественными

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double result = 5 / 2;
+            double result = 5.0 / 2.0;
             Console.WriteLine("5 / 2 = {0}", result);
             Console.ReadKey();
             /*string str = Console.ReadLine();
